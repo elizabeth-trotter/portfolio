b5298b95f5aada9803511d0824df0f80e1ddebe4
et120 <e.trotter120@gmail.com>
2024-04-29 21:36:46 -0700

    use Script Nextjs

--- src/app/components/NavbarComponent.tsx
+++ src/app/components/NavbarComponent.tsx
@@ -1,64 +1,48 @@
-'use client';
-
-import Link from "next/link";
-import { useEffect } from "react";
+import Script from 'next/script';
+import { useEffect } from 'react';
 import { Navbar } from "flowbite-react";
 import { DarkThemeToggle, Flowbite } from "flowbite-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloudArrowDown, faFileDownload, faFilePdf } from "@fortawesome/free-solid-svg-icons";
-
-declare const AdobeDC: any; // Declare AdobeDC as any type
+import { faCloudArrowDown } from "@fortawesome/free-solid-svg-icons";
+declare const AdobeDC: any;
 
 export default function NavbarComponent() {
+  useEffect(() => {
+    // Initialize Adobe DC View SDK when it's ready
+    document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
+      const handleResumePreview = () => {
+        var adobeDCView = new AdobeDC.View({clientId: "bdb06914f3304a4096e0fe55e8c73bbd"});
+        adobeDCView.previewFile({
+          content:{location: {url: "/ElizabethsResume.pdf"}}, // Update this with the path to your PDF file
+          metaData:{fileName: "resume.pdf"}
+        }, {embedMode: "LIGHT_BOX"});
+      };
 
-    useEffect(() => {
-        // Load Adobe DC View SDK script dynamically
-        const script = document.createElement('script');
-        script.src = 'https://documentcloud.adobe.com/view-sdk/main.js';
-        script.async = true;
-        document.body.appendChild(script);
-    
-        // Initialize Adobe DC View SDK when it's ready
-        script.addEventListener('load', () => {
-          document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-            const handleResumePreview = () => {
-              var adobeDCView = new AdobeDC.View({clientId: "bdb06914f3304a4096e0fe55e8c73bbd"});
-              adobeDCView.previewFile({
-                content:{location: {url: "/ElizabethsResume.pdf"}}, // Update this with the path to your PDF file
-                metaData:{fileName: "resume.pdf"}
-              }, {embedMode: "LIGHT_BOX"});
-            };
-      
-            // Add event listener to handle resume preview
-            const resumeLink = document.getElementById('resume-link');
-            resumeLink?.addEventListener('click', handleResumePreview);
-      
-            // Clean up event listener on component unmount
-            return () => {
-              resumeLink?.removeEventListener('click', handleResumePreview);
-            };
-          });
-        });
-    
-        // Clean up script tag on component unmount
-        return () => {
-          document.body.removeChild(script);
-        };
-      }, []);
-      
+      // Add event listener to handle resume preview
+      const resumeLink = document.getElementById('resume-link');
+      resumeLink?.addEventListener('click', handleResumePreview);
+
+      // Clean up event listener on component unmount
+      return () => {
+        resumeLink?.removeEventListener('click', handleResumePreview);
+      };
+    });
+  }, []);
+
   return (
     <Flowbite>
-        <Navbar fluid rounded className="bg-[#faf0e6] font-josefin-sans">
+      <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
+      <Navbar fluid rounded className="bg-[#faf0e6] font-josefin-sans">
         <Navbar.Toggle />
         <Navbar.Collapse>
-            <Navbar.Link href="#about" active>About</Navbar.Link>
-            <Navbar.Link href="#projects">Projects</Navbar.Link>
-            <Navbar.Link href="#skills">Skills</Navbar.Link>
-            <Navbar.Link href="#contact">Contact</Navbar.Link>
-            <Navbar.Link id="resume-link">Resume <span><FontAwesomeIcon icon={faCloudArrowDown} height={14}/></span></Navbar.Link>
+          <Navbar.Link href="#about" active>About</Navbar.Link>
+          <Navbar.Link href="#projects">Projects</Navbar.Link>
+          <Navbar.Link href="#skills">Skills</Navbar.Link>
+          <Navbar.Link href="#contact">Contact</Navbar.Link>
+          <Navbar.Link id="resume-link">Resume <span><FontAwesomeIcon icon={faCloudArrowDown} height={14}/></span></Navbar.Link>
         </Navbar.Collapse>
         <DarkThemeToggle />
-        </Navbar>
+      </Navbar>
     </Flowbite>
   );
 }
