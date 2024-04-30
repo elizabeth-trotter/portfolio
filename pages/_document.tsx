fd23d4aab619e6a43898804b1ad1d306a613eb09
et120 <e.trotter120@gmail.com>
2024-04-29 21:57:56 -0700

    async script src _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,5 @@
+// pages/_document.tsx
+
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -5,8 +7,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Include the Adobe DC View SDK script here */}
-          <script src="https://documentcloud.adobe.com/view-sdk/main.js" />
+          {/* Add your meta tags, stylesheets, etc. */}
+          <script async src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
         </Head>
         <body>
           <Main />
